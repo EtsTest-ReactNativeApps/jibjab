431995f4bdc298e0b3a6ad390593d7819b5c5a6b
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-16 14:38:07 -0500

    Incorporated GiftedChat with static messages
    Also moved the code setting title to name into componenDidMount (out of render) to eliminate warning of attempting to change a component inside render function of another component

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,84 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 export default class Chat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: `Hello, ${this.props.route.params.name}. Welcome to the chat!`,
+          createdAt: new Date(),
+          system: true,
+        },
+        {
+          _id: 2,
+          text: "Good morning!",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "Greg Vincent",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+        }}
+      />
+    );
+  }
+
   render() {
     //access name and color props from StartScreen
-    let { name, color } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name, backgroundColor: color });
+    let { color } = this.props.route.params;
 
     return (
       <View style={[styles.container, { backgroundColor: color }]}>
-        <Text style={styles.welcomeText}>Hello from ChatScreen</Text>
+        {/* <Text style={styles.welcomeText}>Hello from ChatScreen</Text> */}
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {/* For android os only so that pop-up keyboard doesn't block view of chat input */}
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
